87a63857ba47a94ecd6e20c9d570d3e458fa3ce0
Kyle Johnsen <kaelstrom@gmail.com>
2012-11-14 15:58:12 -0500

    added basic camera, todolist

--- src/main.py
+++ src/main.py
@@ -6,13 +6,16 @@ import random
 import time
 import numpy
 import math
+from math import fmod, sin, cos
 
 #These three defines exist in OpenGL.GL, but does not correspond to those used here
 GL_GEOMETRY_SHADER_EXT       = 0x8DD9
 GL_GEOMETRY_INPUT_TYPE_EXT   = 0x8DDB
 GL_GEOMETRY_OUTPUT_TYPE_EXT  = 0x8DDC
 GL_GEOMETRY_VERTICES_OUT_EXT = 0x8DDA
-
+cam_r=150
+cam_theta=270
+cam_phi=180
 
 # Define the missing glProgramParameteri method
 _glProgramParameteri = None
@@ -59,7 +62,7 @@ void main(){
 
 geom = '''#version 120 
 #extension GL_EXT_geometry_shader4 : enable 
-const float radius = 0.05;
+const float radius = 0.5;
 varying out vec2 coord;
 
 void main() 
@@ -106,7 +109,7 @@ def my_idle( ):
     global delta 
     t = time.clock()
     passed = t-delta
-    theta += passed
+    #theta += passed
     delta = t
     global POINTS
     #POINTS+=.1*(.5-numpy.random.random( cube_length**3*3 ).reshape( (-1,3) ))
@@ -151,16 +154,18 @@ def reshape( width, height ):
 
 POINTS = None
 COLORS = None
+STATE  = None
 def display( ):   
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )  
    glLoadIdentity()   
-   gluLookAt( 60.0,60.0,25.0,
-              0.0,0.0,0.0,
-              0.0,0.0,1.0 )
-  
+   #gluLookAt( 100.0,100.0,100.0,
+   #           0.0,0.0,0.0,
+   #           0.0,0.0,1.0 )
+   
+   gluLookAt(eyeX,eyeY,eyeZ,25,25,25,upX, upY, upZ);
 
    glRotate( theta*15.0, 0.0,0.0,1.0 )
-   glTranslatef( 25.0,25.0,0 )
+   #glTranslatef( 25.0,25.0,0 )
    
    #glEnable( GL_BLEND )
    glBlendFunc( GL_SRC_ALPHA, GL_ONE )
@@ -174,15 +179,51 @@ def display( ):
    glutSwapBuffers()
 
 
+eyeX=eyeY=eyeZ=upX=upY=upZ=0
+def onMouseMove(x, y):
+    global eyeX,eyeY,eyeZ,upX,upY,upZ,cam_theta,cam_phi,cam_r
+    # Mouse point to angle conversion
+    cam_theta = (360.0/winHeight)*y*3.0    #3.0 rotations possible
+    cam_phi = (360.0/winWidth)*x*3.0
+
+    # Restrict the angles within 0~360 deg (optional)
+    if cam_theta > 360:
+        cam_theta = fmod(cam_theta,360.0)
+    if cam_phi > 360:
+        cam_phi = fmod(cam_phi,360.0)
+
+    # Spherical to Cartesian conversion.   
+    # Degrees to radians conversion factor 0.0174532
+    eyeX = cam_r * sin(cam_theta*0.0174532) * sin(cam_phi*0.0174532)
+    eyeY = cam_r * cos(cam_theta*0.0174532)
+    eyeZ = cam_r * sin(cam_theta*0.0174532) * cos(cam_phi*0.0174532)
+
+    # Reduce theta slightly to obtain another point on the same longitude line on the sphere.
+    dt=1.0
+    eyeXtemp = cam_r * sin(cam_theta*0.0174532-dt) * sin(cam_phi*0.0174532)
+    eyeYtemp = cam_r * cos(cam_theta*0.0174532-dt)
+    eyeZtemp = cam_r * sin(cam_theta*0.0174532-dt) * cos(cam_phi*0.0174532)
+
+    # Connect these two points to obtain the camera's up vector.
+    upX=eyeXtemp-eyeX
+    upY=eyeYtemp-eyeY
+    upZ=eyeZtemp-eyeZ
+
+    glutPostRedisplay()
+
+winHeight=720
+winWidth=1280
+
 def init():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE )
-    glutInitWindowSize(1280, 720)
+    glutInitWindowSize(winWidth, winHeight)
     # Open a window
     glutCreateWindow("Glut test window")
     
     glutReshapeFunc( reshape )
     glutDisplayFunc( display )
+    glutMotionFunc( onMouseMove )
     glutIdleFunc( my_idle )
     glEnable( GL_DEPTH_TEST )
     glClearColor( 1,1,1,0 )
@@ -192,18 +233,17 @@ def init():
 
     global POINTS
     global COLORS
-    COLORS =numpy.random.random( cube_length**3*3 ).reshape( (-1,3) )
-    POINTS = 100*numpy.ones( cube_length**3*3 ).reshape( (-1,3) )  
-    
-    c=0
+    global STATE
+    COLORS = numpy.ones( cube_length**3*3 ).reshape( (-1,3) )
+    POINTS = numpy.ones( cube_length**3*3 ).reshape( (-1,3) )
+    STATE  = numpy.ones( cube_length**3*3 ).reshape( (-1,3) )  
+
     for i in range(cube_length):
         for j in range(cube_length):
             for k in range(cube_length):
                 #POINTS[i*cube_length**2+j*cube_length+k] = [i+i*j,j+j*k,k+k*i]
                 POINTS[i*cube_length**2+j*cube_length+k] = [i,j,k]
-                c+=1
-                
-    POINTS*=.02
+                COLORS[i*cube_length**2+j*cube_length+k] = [i*1.0/cube_length,j*1.0/cube_length,k*1.0/cube_length]
      
     glutMainLoop();
 
